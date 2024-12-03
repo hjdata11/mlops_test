2e326b5330e4fc9ca31fb2c46127419a6408dd8a
Hunjin Chung <hjdata11@naver.com>
2024-12-03 09:50:49 +0900

    hello to fastapi1

--- test/test_main.py
+++ test/test_main.py
@@ -6,10 +6,14 @@ from src.main import app
 def client():
     return TestClient(app)
 
+@app.get("/")
+def hello_world():
+    return "FastAPI World!!"
+
 def test_hello_world(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.stauts_code != 500
-    assert response.json() == "Hello World!"
+    assert response.json() == "FastAPI World!!"
     
     
